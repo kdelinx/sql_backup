2ddab486a0ad4eab3398965ec11cd58c3e0f0613
kdelinx <kdelinx@gmail.com>
2013-08-26 15:46:15 +0400

    Update mysql_dump.sh

--- mysql_dump.sh
+++ mysql_dump.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-###################################
-#This script make for exec backup #
-#Develope by Afonin Artem         #
-#by nickname of kdelinx           #
-###################################
+####################################
+# This script make for exec backup #
+# Develope by Afonin Artem         #
+# by nickname of kdelinx           #
+####################################
+
+mkdir -p $PATH
 
 MYSQL = 'which mysqlump';
 if [[ $(whoami) == root ]]; then
@@ -12,6 +14,6 @@ if [[ $(whoami) == root ]]; then
   read PASS;
 fi
 DATE='date --date="today" +%F'
-PATH = '/var/backup/mysql'
+PATH='/var/backup/mysql'
 
 $MYSQL -u root -p $PASS --all-databases >  $PATH/mysql-$DATE-backup.sql
